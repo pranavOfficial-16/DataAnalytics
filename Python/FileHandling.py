9171565dbcd927e16da1ec765ed4bcf6f53c96a1
PRANAV <pranav.g@stryker.com>
2023-09-10 13:07:26 +0530

    commit

--- Python/FileHandling.py
+++ Python/FileHandling.py
@@ -1,38 +1,38 @@
 # read file
-f = open("funny.txt", "r")
+f = open("./Data/funny.txt", "r")
 for line in f:
     print(line)
 f.close()
 
 # with statement
-with open("funny.txt", "r") as f:
+with open("./Data/funny.txt", "r") as f:
     for line in f:
         print(line)
 
 # readlines()
-with open("funny.txt", "r") as f:
+with open("./Data/funny.txt", "r") as f:
     lines = f.readlines()
     print(lines)
 
 # write file
-with open("love.txt", "w") as f:
+with open("./Data/funny.txt", "w") as f:
     f.write("I love python")
 
 # same file when you write i love javascript the previous line goes away
-with open("love.txt", "w") as f:
+with open("./Data/love.txt", "w") as f:
     f.write("I love python")
 
 # You can use append mode to stop having previous lines overwritten
-with open("love.txt", "a") as f:
+with open("./Data/love.txt", "a") as f:
     f.write("I love python")
 
 # writelines
-with open("love.txt", "w") as f:
+with open("./Data/love.txt", "w") as f:
     f.writelines(["I love C++\n", "I love scala"])
 
 # program to perform data analytics on cricket player's performance
 player_scores = {}
-with open("scores.csv", "r") as f:
+with open("./Data/scores.csv", "r") as f:
     for line in f:
         player,score = line.split(',')
         score = int(score)
