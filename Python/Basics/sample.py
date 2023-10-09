c98febe7189d0021848d0661631ef54ea0889629
PRANAV <pranav.g@stryker.com>
2023-10-09 11:23:02 +0530

    commit

--- Python/Basics/sample.py
+++ Python/Basics/sample.py
@@ -2,3 +2,14 @@ lis = "\n Geeks for Geeks \n"
 string = lis.strip()
 print(string)
 
+for i in range(10):
+    print(i)
+
+s = "(0010,0010),"
+s = s[:-1]
+print(s)
+
+text = ["pranav","rao"]
+str_text = ''.join(text)
+print(str_text)
+
